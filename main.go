8853b07cf326b8ab6c5899babbe7a90d694e6784
Andrey Bubenko <a.bubenko@mail.ru>
2023-03-04 13:31:22 +0700

    fix

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/STGPSYCHO/GO/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	route := gin.Default()
+
+	models.ConnectDB()
 
 	route.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "Hello World!"})
--- models/database.go
+++ models/database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := "host=localhost user=postgres password=1010 dbname=Go_Bacnkend port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=localhost user=gorm password=gorm dbname=Go_Bacnkend port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Не удалось подключиться к базе данных")
