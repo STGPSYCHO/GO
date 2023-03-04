0ff9312caad900baf21585405bd9eb78e7ee41ea
Andrey Bubenko <a.bubenko@mail.ru>
2023-03-04 13:24:13 +0700

    set up server

---
+++ models/database.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=1010 dbname=Go_Bacnkend port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Не удалось подключиться к базе данных")
+	}
+	db.AutoMigrate(&User{})
+
+	return db
+}
